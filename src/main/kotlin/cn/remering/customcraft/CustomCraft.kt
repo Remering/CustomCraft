3567b8357c954a430e2ed4f7f04b809cf2c26b17
remering <remering@163.com>
2023-08-30 20:57:44 +0800

    feature: 当插件重载时关闭所有gui

--- src/main/kotlin/cn/remering/customcraft/CustomCraft.kt
+++ src/main/kotlin/cn/remering/customcraft/CustomCraft.kt
@@ -8,12 +8,14 @@ import cn.remering.customcraft.registry.registerRecipePredicateBuilders
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import mc.obliviate.inventory.InventoryAPI
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class CustomCraft : JavaPlugin() {
 
     companion object {
         lateinit var self: CustomCraft
+        lateinit var inventoryApi: InventoryAPI
     }
 
     override fun onLoad() {
@@ -27,12 +29,18 @@ class CustomCraft : JavaPlugin() {
 
     override fun onEnable() {
         CommandAPI.onEnable()
-        InventoryAPI(this).init()
+        inventoryApi = InventoryAPI(this).apply {
+            init()
+        }
         registerCommands()
         Config.load()
     }
 
     override fun onDisable() {
+        Bukkit.getOnlinePlayers()
+            .mapNotNull { inventoryApi.getPlayersCurrentGui(it) }
+            .filter { !it.isClosed }
+            .forEach { it.player.closeInventory() }
         unregisterCommands()
         CommandAPI.onDisable()
     }
